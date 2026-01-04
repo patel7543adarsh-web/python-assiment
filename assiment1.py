68a8f9e78cb6aa1d06036e223805047fac949991
adarshpatel-code craft <adarshpatelid2005@gmail.com>
2026-01-04 10:21:37 +0530

    my first upload form vs code

---
+++ assiment1.py
@@ -0,0 +1,15 @@
+# mathemetical operation
+num1 = float(input("Enter first number:"))
+num2 = float(input("Enter second number:"))
+
+add_result = num1 + num2
+sub_result = num1 - num2
+mul_result = num1 * num2
+div_result = num1 / num2 
+
+print (f"Addition:{num1} + {num2} = {add_result}")
+print(f"Substraction:{num1} - {num2} ={sub_result}")
+print(f"Multiplication:{num1} * {num2} = {mul_result}")
+print(f"Division:{num1} / {num2} {div_result}")
+
+
