9d20aeee713dc501257c5928e41ee504142e84eb
adarshpatel-code craft <adarshpatelid2005@gmail.com>
2026-02-22 08:10:11 +0530

    math module

---
+++ Module Second/Module Third/assiment1.1.py
@@ -0,0 +1,30 @@
+# Task 2: Using the Math Module for Calclution
+
+# problem statement: Write a python program that:
+# 1.Ask me user for a number as input.
+# 2.Use the math module to calclute the:
+      # squre root of number
+      # natural logarithm (log base e) of the number
+      # sine of the number (in radian)
+# 3.Display the calclute the result:
+# Expeted output:
+# for example, if the user enters the 25 ,the output should be:
+   # Enter a number: 25
+   # Square root: 5.0
+   # logarithm: 3.2188758
+   # sine: -o.1323517
+
+import math
+def calculate_square_root(num):
+    return math.sqrt(num)
+def calculate_natural_log(num):
+    return math.log(num)
+def calculate_sine(num):
+    return math.sin(num)
+
+number = float(input("Enter a number: "))
+
+print("square root:", calculate_square_root(number))
+print("logarithm :", calculate_natural_log(number))
+print("sine:", calculate_sine(number))
+    
